6903c95e435e7561cc42718c55201d8c5d9c2980
aiden-jung <jimmy53120488@gmail.com>
2020-08-11 10:44:57 +0900

    VANILLA: redux 사용해서 todo list add 로직 완료

--- src/index.js
+++ src/index.js
@@ -4,17 +4,43 @@ const form = document.getElementById("todoForm");
 const input = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
 
-const addTodoList = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  todoList.appendChild(li);
+const ADD_LIST = "ADD_LIST";
+const DELETE_LIST = "DELETE_LIST";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_LIST:
+      return [...state, { text: action.text, id: action.id }];
+    case DELETE_LIST:
+      return [];
+    default:
+      return state;
+  }
 };
 
-const handleAddTodo = event => {
+const store = createStore(reducer);
+
+const paintToDo = () => {
+  const toDos = store.getState();
+  todoList.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    li.innerText = toDo.text;
+    todoList.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDo);
+
+const handleTodoSubmit = event => {
   event.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addTodoList(toDo);
+  store.dispatch({
+    type: ADD_LIST,
+    text: toDo,
+    id: Date.now()
+  });
 };
 
-form.addEventListener("submit", handleAddTodo);
+form.addEventListener("submit", handleTodoSubmit);
